bb334054d693b152cf1fe8fce9f093e3bf464744
rikun-patel <rikun.patel@devdigital.com>
2022-07-18 12:24:18 +0530

    Initial Commit

--- lib/src/resources/app_colors.dart
+++ lib/src/resources/app_colors.dart
@@ -8,7 +8,6 @@ class AppColors {
   static const Color black = Color(0xff000000);
   static const Color whiteColor = Color(0xFFffffff);
   static const Color clockFormatSelectionColor = Color(0xff5b5c62);
-  //////
   static const Color textBoxBackgroundColorLight = Color(0xFFFFFFFF);
   static const Color textBoxBackgroundColorDark = Color(0xff57585B);
 
